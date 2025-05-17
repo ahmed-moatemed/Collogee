c3b5ba0128c41e5f6c3a746c8e4386f1acb00640
Ahmed Moatemed <https://githup.com/ahmed-moatemed>
2025-05-17 21:07:19 +0300

    hide

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -7,9 +7,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // Replace with your Supabase URL and anon key
-const SUPABASE_URL = 'https://ndidfnjsazqnoyfrdwxu.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5kaWRmbmpzYXpxbm95ZnJkd3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc0MzY1NjUsImV4cCI6MjA2MzAxMjU2NX0.XAdttpwLnoz9loSjWAOsDf3e9PtiqRO32A-hprF9qRo';
-
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 // Initialize the Supabase client
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
